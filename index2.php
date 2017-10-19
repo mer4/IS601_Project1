f6639ad19541700768073ceec649dba613d7719f
michael e rogowski wis student <mer4@afsconnect2.njit.edu>
2017-10-19 18:26:00 -0400

    final commit today

--- index2.php
+++ index2.php
@@ -1,32 +1,48 @@
 <?php
+
 //set error reporting on
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-//gets name of file from URL
-$name = $_REQUEST['file'];
+$obj = new main();
 
-$handle = fopen($name, "r");
-echo '<table>';
-//displays header row
-if (TRUE) {
-	$csvcontents = fgetcsv($handle);
-	echo '<tr>';
-	foreach ($csvcontents as $headercolumn) {
-		echo "<th>$headercolumn</th>";
+class main {
+
+	public function __construct() {
+
+		//gets name of file from URL
+		$name = $_REQUEST['file'];
+
+		$handle = fopen($name, "r");
+		stringFunctions::printOut("<html><body><table>");
+		//displays header row
+		if (TRUE) {
+			$filecontents = fgetcsv($handle);
+			stringFunctions::printOut("<tr>");
+			foreach ($filecontents as $headercolumn) {
+				stringFunctions::printOut("<th>$headercolumn</th>");
+			}
+			stringFunctions::printOut("</tr>");
+		}
+		//displays other rows
+		while ($filecontents = fgetcsv($handle)) {
+			stringFunctions::printOut("<tr>");
+			foreach ($filecontents as $column) {
+				stringFunctions::printOut("<td>$column</td>");
+			}
+			stringFunctions::printOut("</tr>");
+		}
+		stringFunctions::printOut("</table></body></html>");
+		fclose($handle);
 	}
-	echo '</tr>';
 }
-//displays other rows
-while ($csvcontents = fgetcsv($handle)) {
-	echo '<tr>';
-	foreach ($csvcontents as $column) {
-		echo "<td>$column</td>";
+
+class stringFunctions {
+
+        static public function printOut($string) {
+		echo $string;
 	}
-	echo '</tr>';
 }
-echo '</table>';
-fclose($handle);
 
 /*
 basic way of creating a table

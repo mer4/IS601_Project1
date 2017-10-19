77c81b6e94e71703517bb402e14bcb76659dd85c
michael e rogowski wis student <mer4@afsconnect2.njit.edu>
2017-10-19 14:09:04 -0400

    reformatted form file

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
+<!DOCTYPE html>
 <html>
 <body>
 
 <form action="index.php" method="post" enctype="multipart/form-data">
-Select CSV file to upload:
-<input type="file" name="fileToUpload" id="fileToUpload">
-<input type="submit" value="Upload File" name="submit">
+	Select CSV file to upload:
+	<input type="file" name="fileToUpload" id="fileToUpload">
+	<input type="submit" value="Upload File" name="submit">
 </form>
 
 </body>
 </html>
 
-<?php 
+<?php
+
 //set error reporting on
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
@@ -23,14 +25,15 @@ if(isset($_POST["submit"])) {
 	if ($file_ext!="csv") {
 		echo "Please upload a CSV file.";
 	} else {
-		if(file_exists($target_file)) {
+		if (file_exists($target_file)) {
 			echo "The file already exists.";
 		} else {
 			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
          			echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+				$pageName = "2";
 				header("Location:index2.php?page=$pageName&file=$target_file");
 			} else {
-		        	echo "Sorry, there was an error uploading your file.";
+				echo "Sorry, there was an error uploading your file.";
 			}
 		}
 	}
